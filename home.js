996907a511e18d12229658f4270ff22d96db85c3
Mahi soni <167469698+639251@users.noreply.github.com>
2025-07-30 12:06:41 +0530

    Update home.js

--- home.js
+++ home.js
@@ -3,3 +3,15 @@ document.getElementById('mobile-menu').addEventListener('click', function () {
   const nav = document.getElementById('nav-links');
   nav.classList.toggle('active');
 });
+ document.querySelectorAll('.read-more-btn').forEach(button => {
+    button.addEventListener('click', () => {
+      const card = button.closest('.project-card');
+      card.classList.toggle('expanded');
+
+      if (card.classList.contains('expanded')) {
+        button.textContent = 'Read Less';
+      } else {
+        button.textContent = 'Read More';
+      }
+    });
+  });
